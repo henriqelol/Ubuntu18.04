f27c9dfc75e5b32741eedd8355c03e47c5e9ef50
henriqelol <eduardo-hnq@hotmail.com>
2019-02-08 14:12:47 -0200

    update install

--- install.sh
+++ install.sh
@@ -4,18 +4,6 @@ echo ""
 
 echo "Ver versão do Sistema ? [S][N]"
 read versao
-<<<<<<< HEAD
-["$versao" == "S"] && sh versao.sh
-
-echo -e "[1] Executar por etapas\n[2] Executar todos comandos/programas de uma vez"
-read saida
-if [$saida == 1];
-then
-echo "!!--INSTALAÇÃO FINALIZADA--!!"
-else
-echo "Execução completa"
-echo "!!--INSTALAÇÃO FINALIZADA--!!"
-=======
 [ "$versao" == "S" ] && sh versao.sh
 
 echo -e "[1] Executar por etapas\n[2] Executar todos comandos/programas de uma vez"
