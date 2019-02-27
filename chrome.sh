44f0ebf8d2f7737d5e2e20d0c7075abc129906ae
henriqelol <eduardo-hnq@hotmail.com>
2019-02-27 09:56:15 -0300

    upgrade chrome

--- chrome.sh
+++ chrome.sh
@@ -1,3 +1,4 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 cp /usr/share/applications/google-chrome-stable /home/'hostname'/Área\ de\ Trabalho/
+echo "google-chrome-stable hold" | sudo dpkg --set-selections
