affde9c2fb44fa16a95ac1696fdd6f0fe0059da3
henriqelol <eduardo-hnq@hotmail.com>
2019-09-03 12:58:11 -0300

    Não estava instalando o sublime

--- editores.sh
+++ editores.sh
@@ -3,5 +3,7 @@ apt-get update
 apt -qy install vim
 #Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
 sudo apt-get install sublime-text
