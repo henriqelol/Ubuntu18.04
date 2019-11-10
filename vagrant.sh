1fc36b4e922e666878ca0f064a0e55cf3160468a
Eduardo Henrique <eduardo-hnq@hotmail.com>
2019-11-10 16:54:10 -0300

    Upgrade release Vagrant
    Up Relea

--- vagrant.sh
+++ vagrant.sh
@@ -1,4 +1,4 @@
-wget https://releases.hashicorp.com/vagrant/2.2.3/vagrant_2.2.3_x86_64.deb
-dpkg -i vagrant_2.2.3_x86_64.deb 
+wget https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb
+dpkg -i vagrant_2.2.6_x86_64.deb 
 vagrant -v
 which vagrant
