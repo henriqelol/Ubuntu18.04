5cc9a8f06da13f83e6032efdf4f6246a083880ef
Eduardo Henrique <eduardo-hnq@hotmail.com>
2020-03-17 11:42:20 -0300

    Update virtualbox.sh

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,3 +1,5 @@
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list'
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+apt-get update
+apt-get install virtualbox-6.1 -y
