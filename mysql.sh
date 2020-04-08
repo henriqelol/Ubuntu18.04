ceb68820d3f6f0df33f5044a307be0376b6ced76
Eduardo Henrique <eduardo-hnq@hotmail.com>
2020-04-07 23:20:25 -0300

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,9 @@
+sudo apt-get remove --purge mysql*  
+sudo apt-get purge mysql*  
+sudo apt-get autoremove  
+sudo apt-get autoclean  
+sudo apt-get remove dbconfig-mysql
+
 sudo apt update
 sudo apt install mysql-server
 
